7dc25831db138b72ef37514d151721a0fa27ec4f
eddyhwang97 <hwangdw1997@gmail.com>
2025-03-26 15:17:04 +0900

    [3/n] Create TodoItemInputField: Add Button to TodoItemInputField component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import "./App.css";
 
-import { useState } from 'react';
-import TextField from '@mui/material/TextField';
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
 const TotoItemInputField = (props) => {
-const [input, setInput] = useState();
+  const [input, setInput] = useState();
 
-  return <div>
-    <TextField
-    id="todo-item-input"
-    label="todo Item"
-    variant="outlined"
-    onChange={(e)=>setInput(e.target.value)}
-    value={input}
-    />
-  </div>;
+  return (
+    <div>
+      <TextField id="todo-item-input" label="todo Item" variant="outlined" onChange={(e) => setInput(e.target.value)} value={input} />
+        <Button variant="outlined">submit</Button>
+    </div>
+  );
 };
 function App() {
-  return <div className="App">
-    <TotoItemInputField />
-  </div>;
+  return (
+    <div className="App">
+      <TotoItemInputField />
+    </div>
+  );
 }
 
 export default App;
