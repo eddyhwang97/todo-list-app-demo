38ae493f2dc10495056e9dbfdc1e688f3b795bd5
eddyhwang97 <hwangdw1997@gmail.com>
2025-03-26 15:15:06 +0900

    [2/n] Create TodoItemInputField: Add TextField to TodoItemInputField component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+
 const TotoItemInputField = (props) => {
-  return <div></div>;
+const [input, setInput] = useState();
+
+  return <div>
+    <TextField
+    id="todo-item-input"
+    label="todo Item"
+    variant="outlined"
+    onChange={(e)=>setInput(e.target.value)}
+    value={input}
+    />
+  </div>;
 };
 function App() {
   return <div className="App">
