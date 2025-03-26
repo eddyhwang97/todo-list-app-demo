f2db6f0a455914b3a0d2a2987c6e71565a31d8e4
eddyhwang97 <hwangdw1997@gmail.com>
2025-03-26 16:20:44 +0900

    [2/n] Add TodoItemList react component to display todo items: Make TodoItemList component to take an array of todo items

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 
+// TotoItemInputField :  Todo아이템 입력할 컴포넌트
 const TotoItemInputField = (props) => {
   const [input, setInput] = useState();
-
+  // onSubmit : submit버튼 클릭시 택스트필드에 있는 text 전송
   const onSubmit = () => {
     props.onSubmit(input);
     setInput("");
@@ -21,15 +22,27 @@ const TotoItemInputField = (props) => {
     </div>
   );
 };
+
+// TotoItemLIst :  등록된 아이템 보여줄 컴퍼넌트
 const TotoItemLIst = (props) => {
-  return <div></div>;
+  const todoList = props.todoItemList.map((todoItem, index) => {
+    return <li key={index}>{todoItem.todoItemContent}</li>;
+  });
+  return (
+    <div>
+      <ul>{todoList}</ul>
+    </div>
+  );
 };
+
+// app 컴포넌트
 function App() {
   return (
     <div className="App">
       {/* TotoItemInputField/버튼 눌렀을때  onSubmit callback 콜해주기  */}
       <TotoItemInputField onSubmit={() => {}} />
-      <TotoItemLIst />
+        {/* TodoItemList 콤포넌트 props 로 등록된 Todo 아이템들 받기  */}
+      <TotoItemLIst todoItemList={[]} />
     </div>
   );
 }
